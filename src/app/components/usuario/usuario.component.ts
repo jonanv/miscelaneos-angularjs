50441fb1b2fd6439bcb6288a5f30b1559cd95de3
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-03-31 21:30:32 -0500

    Se caputaron paramtros de url desde el componente padre y desde el componente hijo con activatedRoute y con parent desde el hijo

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.activatedRoute.params
+    .subscribe(params => {
+      console.log(params['id']);
+    });
+  }
 
   ngOnInit() {
+
   }
 
 }
